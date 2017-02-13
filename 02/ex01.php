a8cbf4b988dcab5acf3bd496a0a7464857926268
Denys Kharchenko <denys.d.kharchenko@gmail.com>
2017-02-13 02:47:02 +0200

    rearrange files and second lesson

---
+++ 02/ex01.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Практикум №1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body><h1>Практикум №1 - "Вывод"</h1>
+<a href=".">вернуться</a>
+<hr>
+
+<?php
+
+// Однострочный комментарий
+echo 'Вывод информации <br>'; // Однострочный комментарий до конца строки
+echo 'строка1<br>', 'строка2<br>', 'строка3<br>'; # Другой однострочный комментарий до конца строки
+
+echo ('Еще строка<br>');
+//echo ('ошибка1<br>', 'ошибка1<br>', 'ошибка1<br>');
+
+print 'print<br>';
+print('other print<br>');
+// print 'ошибка1<br>', 'ошибка2<br>', 'ошибка3<br>';
+// print ('ошибка1<br>', 'ошибка2<br>', 'ошибка3<br>');
+
+print_r(Array('arr1', 'arr2', 'arr3', 'arr4', 'arr5'));
+
+?>
+
+</body>
+</html>
